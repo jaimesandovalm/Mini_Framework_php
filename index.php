3c045023937103b9ffcf5998854c565d6cf60284
jaimesandovalm <jaimesandovalm1990@gmail.com>
2022-11-30 12:12:13 -0300

    Configuracion del Index

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
-<h1>Mini Framework</h1>
+<?php
+
+$ruta = !empty($_GET['url']) ? $_GET['url'] : "home/index";
+$array = explode("/", $ruta);
+$controller = $array[0];
+$metodo = "index";
+$parametro = "";
+
+if (!empty($array[1])) {
+    if(!empty($array[1] != '')){
+        $metodo = $array[1];
+    }
+}
+
+if (!empty($array[2])) {
+    if(!empty($array[2] != '')){
+        for ($i=2; $i < count($array) ; $i++) { 
+            $parametro .= $array[$i]. ",";
+        }
+        $parametro = trim($parametro, ",");
+    }
+}
+
+$dircontroller = "Controllers" . $controller . ".php";
+
+if (file_exists($dircontroller)) {
+    require_once $dircontroller;
+    $controller = new $controller();
+    if (metod_exists($contoller,$metodo)) {
+        $controller->$metodo($parametro);
+    }else{
+        echo "no existe el metodo";
+    }
+}else{
+    echo "no existe el controlador";
+}
